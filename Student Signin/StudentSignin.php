2df42b43894678faa4cb31c078b167aa12c71742
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-16 17:04:14 +0800

    update here'

--- Student Signin/StudentSignin.php
+++ Student Signin/StudentSignin.php
@@ -1,82 +1,67 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_nt3102";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get input data
+    $sr_code = $_POST["sr_code"];
+    $password = $_POST["password"];
+
+    // Check if user exists
+    $check_user_query = "SELECT * FROM student WHERE Sr_code = '$sr_code'";
+    $result = $conn->query($check_user_query);
+
+    if ($result->num_rows > 0) {
+        // User exists, check password
+        $row = $result->fetch_assoc();
+        if ($password == $row["Password"]) {
+            echo "Login successful!";
+            // Add your login logic here
+        } else {
+            echo "Incorrect password!";
+        }
+    } else {
+        // User does not exist, insert new user
+        $insert_user_query = "INSERT INTO student (Sr_code, Password) VALUES ('$sr_code', '$password')";
+        if ($conn->query($insert_user_query) === TRUE) {
+            echo "Sign up successful!";
+            // Add your login logic here
+        } else {
+            echo "Error: " . $insert_user_query . "<br>" . $conn->error;
+        }
+    }
+}
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Login System</title>
 </head>
 <body>
-    <h2>Login</h2>
-
-    <?php
-    // PHP code for handling form submission
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $srCode = $_POST["sr_code"];
-        $password = $_POST["password"];
-
-        // Validate the login
-        if (loginUser($srCode, $password)) {
-            echo "<p>Login successful!</p>";
-        } else {
-            echo "<p>Login failed. Please check your Sr_code and password.</p>";
-        }
-    }
-
-    function loginUser($srCode, $password) {
-        // Implement your database connection
-        $servername = "localhost";
-        $dbname = "db_nt3102";
-        $username_db = "Sr_code";
-        $password_db = "password";
-
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username_db, $password_db);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            // Check if the Sr_code exists in the database
-            $stmt = $conn->prepare("SELECT * FROM Student WHERE Sr_code = :srCode");
-            $stmt->bindParam(':srCode', $srCode);
-            $stmt->execute();
-
-            if ($stmt->rowCount() > 0) {
-                // Sr_code exists, check the password
-                $row = $stmt->fetch(PDO::FETCH_ASSOC);
-                if ($row['password'] === $password) {
-                    return true; // Login successful
-                }
-            } else {
-                // Sr_code doesn't exist, create a new account
-                $stmt = $conn->prepare("INSERT INTO Student (Sr_code, password) VALUES (:srCode, :password)");
-                $stmt->bindParam(':srCode', $srCode);
-                $stmt->bindParam(':password', $password);
-                $stmt->execute();
-
-                return true; // Login successful after account creation
-            }
-
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-            return false;
-        } finally {
-            $conn = null;
-        }
-
-        return false;
-    }
-    ?>
-
+    <h2>Login System</h2>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="sr_code">Sr_code:</label>
-        <input type="text" name="sr_code" required>
-
-        <br>
+        <input type="text" id="sr_code" name="sr_code" required><br>
 
         <label for="password">Password:</label>
-        <input type="password" name="password" required>
+        <input type="password" id="password" name="password" required><br>
 
-        <br>
-
-        <input type="submit" value="Login">
+        <input type="submit" value="Submit">
     </form>
 </body>
 </html>
